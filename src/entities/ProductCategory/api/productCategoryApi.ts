752738b1a603d62c91b83c6d746086bcda77776f
Oleg Esikov <esikov.oleg@mail.ru>
2024-02-25 16:22:32 +0300

    feat: added ProductCategoryFilter in get product categories request

--- src/entities/ProductCategory/api/productCategoryApi.ts
+++ src/entities/ProductCategory/api/productCategoryApi.ts
@@ -2,6 +2,7 @@ import { getAsync, postAsync } from "../../../shared/api/axios";
 import { IProductCategory } from "../model/types/productCategory";
 import { PRODUCT_CATEGORIES_PATH } from "../../../shared/config/api";
 import { ProductCategoryFormFields } from "../model/types/productCategoryFormFields";
+import { ProductCategoryFilter } from "../model/types/productCategoryFilter";
 
 export const addProductCategory = async (productCategory: ProductCategoryFormFields) =>
     await postAsync<ProductCategoryFormFields, IProductCategory>(
@@ -9,5 +10,5 @@ export const addProductCategory = async (productCategory: ProductCategoryFormFie
         productCategory,
     );
 
-export const getProductCategories = async () =>
-    await getAsync<IProductCategory[]>(PRODUCT_CATEGORIES_PATH);
+export const getProductCategories = async (filter?: ProductCategoryFilter) =>
+    await getAsync<IProductCategory[]>(PRODUCT_CATEGORIES_PATH, filter);
---
+++ src/entities/ProductCategory/model/types/productCategoryFilter.ts
@@ -0,0 +1,3 @@
+export type ProductCategoryFilter = {
+    name?: string;
+};
