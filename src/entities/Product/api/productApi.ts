2760afd051a605a72a4e93dbe3d7cd8a7ce31272
Oleg Esikov <esikov.oleg@mail.ru>
2024-03-09 16:20:47 +0300

    feat: added filter by product name

--- src/entities/Product/api/productApi.ts
+++ src/entities/Product/api/productApi.ts
@@ -2,8 +2,10 @@ import { getAsync, postAsync } from "../../../shared/api/axios";
 import { IProduct } from "../model/types/product";
 import { PRODUCTS_PATH } from "../../../shared/config/api";
 import { ProductEditModel } from "../model/types/productEditModel";
+import { ProductFilter } from "../model/types/productFilter";
 
 export const addProduct = async (product: ProductEditModel) =>
     await postAsync<ProductEditModel, IProduct>(PRODUCTS_PATH, product);
 
-export const getProducts = async () => await getAsync<IProduct[]>(PRODUCTS_PATH);
+export const getProducts = async (filter?: ProductFilter) =>
+    await getAsync<IProduct[]>(PRODUCTS_PATH, filter);
